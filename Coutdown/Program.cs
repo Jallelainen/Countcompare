aaf3421da67c0a64f60b9be311944615bfb1a5f6
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-08 09:24:49 +0200

    Add project files.

---
+++ Coutdown/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Coutdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(number > 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while number 0
+        }
+    }
+}
